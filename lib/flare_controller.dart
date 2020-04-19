d65aee0798dac1973dc3eed2933408fd7879b6ee
Sudhansu Bhushan Mishra <sbmishra2000@gmail.com>
2020-04-19 19:40:01 +0530

    Create controller stub

---
+++ lib/flare_controller.dart
@@ -0,0 +1,17 @@
+import 'dart:math';
+
+import 'package:flare_dart/math/mat2d.dart';
+import 'package:flare_flutter/flare.dart';
+import 'package:flare_flutter/flare_actor.dart';
+import 'package:flare_flutter/flare_controller.dart';
+
+class AnimationControls extends FlareController {
+  @override
+  void initialize(FlutterActorArtboard artboard) {}
+
+  @override
+  bool advance(FlutterActorArtboard artboard, double elapsed) {}
+
+  @override
+  void setViewTransform(Mat2D viewTransform) {}
+}
