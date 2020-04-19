34e854e607b89bd7591f43f786fa3068f5d0ebb2
Sudhansu Bhushan Mishra <sbmishra2000@gmail.com>
2020-04-19 19:10:57 +0530

    Make a blank  screen

---
+++ lib/tracking_input.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TrackingInput extends StatefulWidget {
+  @override
+  _TrackingInputState createState() => _TrackingInputState();
+}
+
+class _TrackingInputState extends State<TrackingInput> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
